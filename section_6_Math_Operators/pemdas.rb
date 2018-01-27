b8e1e1b0a3c7ddc2388bed918d4b535f6467d74d
AmadouSallah <amadousallah13@gmail.com>
2018-01-26 16:56:15 -0800

    Completed section 6

---
+++ section_6_Math_Operators/pemdas.rb
@@ -0,0 +1,26 @@
+PEMDAS =
+"P - Parentheses '()'
+E - Exponents '**'
+M - Multiplication '*'
+D - Division '/'
+A - Addition '+'
+S - Substraction '-'"
+
+# PEMDAS: Please Excuse My Dear Aunt Sally
+
+puts PEMDAS
+
+# p 3 + 15 * 23 - 5 / 8**4 + (3 * 2)
+# 3 + 15 * 23 - 5 / 8**4 + 6
+# 3 + 15 * 23 - 5 / 4096 + 6
+# 3 + 345 - 5 / 4096 + 6
+# 3 + 345 - 0 + 6
+# 348 - 0 + 6
+# 356
+
+# Integer (2, 5, 5675)
+# Float (1,2, 1.3, 6.3) #
+# Decimal (2.364589, 5.11457) # accurate, example when building bank account
+
+p 100 / 80 # 1
+p 101 / 80.0 # 1.2625
